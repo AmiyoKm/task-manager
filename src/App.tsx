79bfbd7822d21bb932a4b57e280824ca6a254860
AmiyoKm <omimondol18@gmail.com>
2024-10-21 23:43:33 +0600

    Terminated

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,85 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { AppSidebar } from "./components/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { SidebarProvider } from "./components/ui/sidebar";
 import { LoginForm } from "./components/login-form";
 import MyTask from "./Pages/MyTask";
 import Project from "./Pages/Project";
 import Calender from "./Pages/Calender";
 import Profile from "./Pages/Profile";
 import Settings from "./Pages/Settings";
-import GlobalStateContext from "./context/GlobalContext";
+
 import { Toaster } from "./components/ui/toaster";
 
-function App({ children }: { children: React.ReactNode }) {
+function App() {
   return (
     <>
-   
-      <Routes>
-        
-        <Route
-          path="/"
-          element={
-            <div className="h-screen flex items-center justify-center">
-              <LoginForm />
-            </div>
-          }
-        />
-        <Route
-          path="/Home"
-          element={
-            <SidebarProvider>
-              <AppSidebar />
-              <main>
-                <SidebarTrigger />
-                {children}
-              </main>
-              <Toaster />
-            </SidebarProvider>
-          }
-        />
-        <Route
-          path="/my-tasks"
-          element={
-            <SidebarProvider>
-              <AppSidebar />
-              <MyTask />
-              <main>
-                <SidebarTrigger />
-                {children}
-              </main>
-              
-              <Toaster />
-            </SidebarProvider>
-          }
-        />
-        <Route
-          path="/projects"
-          element={
-            <SidebarProvider>
-              <AppSidebar />
-              <main>
-                <SidebarTrigger />
-                {children}
-              </main>
-              <Toaster />
-              <Project />
-            </SidebarProvider>
-          }
-        />
-        <Route
-          path="/Calender"
-          element={
-            <SidebarProvider>
-              <AppSidebar />
-              <main>
-                <SidebarTrigger />
-                {children}
-              </main>
-              <Calender />
-              <Toaster />
-            </SidebarProvider>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <SidebarProvider>
-              <AppSidebar />
-              <main>
-                <SidebarTrigger />
-                {children}
-              </main>
-              <Profile />
-            </SidebarProvider>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <SidebarProvider>
-              <AppSidebar />
-              <main>
-                <SidebarTrigger />
-                {children}
-              </main>
-              <Settings />
-            </SidebarProvider>
-          }
-        />
-      </Routes>
-     
+      <SidebarProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="h-screen flex items-center justify-center">
+                <LoginForm />
+              </div>
+            }
+          />
+          <Route
+            path="/Home"
+            element={
+              <>
+              <div>Home</div>
+                <AppSidebar />
+              </>
+            }
+          />
+          <Route
+            path="/my-tasks"
+            element={
+              <>
+                <AppSidebar /> <MyTask />
+                <Toaster />
+              </>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <>
+                <AppSidebar />
+                <Project />
+              </>
+            }
+          />
+          <Route
+            path="/Calender"
+            element={
+              <>
+                <AppSidebar />
+                <Calender />
+              </>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <>
+                <AppSidebar />
+                <Profile />
+              </>
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <>
+                <AppSidebar />
+                <Settings />
+              </>
+            }
+          />
+        </Routes>
+      </SidebarProvider>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <GlobalStateContext>
     <BrowserRouter>
       
-        <App children />
+        <App />
       
     </BrowserRouter>
   </GlobalStateContext>
